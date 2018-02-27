941efdbad363ea94ee9d1626c690f45e7adb96cd
chenghui-li <1105689172@qq.com>
2018-02-27 09:52:30 +0800

    二叉树层序遍历

---
+++ 数据结构/二叉树/levelOrder.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<queue>
+using namespace std;
+typedef int ElemType;
+struct BinaryTree{
+	ElemType value;
+	BinaryTree *left;
+	BinaryTree *right;
+};
+void print(int value){
+	printf("%d\n",value);
+}
+void levelOrder(BinaryTree *root){
+	queue<BinaryTree *> q;
+	BinaryTree *bt = root;
+	if(root == nullptr)
+	    return;
+	q.push(root);
+	while(!q.empty()){
+		bt = q.front();
+		print(bt->value);
+		q.pop();
+		if(bt->left != nullptr)
+		    q.push(bt->left);
+		if(bt->right != nullptr)
+		    q.push(bt->right);
+	} 
+}
