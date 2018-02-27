c7ca73b5460601e5748912d476bc2ca946a9337f
chenghui-li <1105689172@qq.com>
2018-02-27 09:16:20 +0800

    二叉树中序遍历 迭代&递归

---
+++ 数据结构/二叉树/InOrder.cpp
@@ -0,0 +1,42 @@
+﻿#include<stdio.h> 
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef int ElemType;
+struct BinaryTree{
+	ElemType value;
+	BinaryTree *left;
+	BinaryTree *right;
+};
+void print(int value){
+	printf("%d\n",value);
+}
+//迭代写法 
+void InOrder(BinaryTree *root){
+	stack<BinaryTree *>s;
+	BinaryTree *bt = root;
+	if(root == nullptr)
+	    return;
+	while(bt!=nullptr || !s.empty()){
+		while(bt!=nullptr){
+			s.push(bt);
+			bt = bt->left;
+		}
+		if(!s.empty()){
+			bt = s.top();
+			print(bt->value);
+			s.pop();
+			bt = bt->right;
+		}
+	}
+}
+
+//递归写法
+void InOrder2(BinaryTree *root) {
+	if(root == nullptr)
+	    return;
+	InOrder2(root->left);
+	print(root->value);
+	InOrder2(root->right);
+}
+
