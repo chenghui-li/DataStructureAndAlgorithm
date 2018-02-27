2930833834a2b110b693d06a7fc549ff9ce981c4
chenghui-li <1105689172@qq.com>
2018-02-27 09:03:43 +0800

    二叉树的先序遍历 递归&迭代

---
+++ 数据结构/二叉树/PreOrder.cpp
@@ -0,0 +1,42 @@
+﻿#include<stdio.h>
+#include<iostream>
+#include<stack>
+using namespace std;
+typedef int ElemType;
+struct BinaryTree{
+	ElemType value;
+	BinaryTree *left;
+	BinaryTree *right;
+};
+void print(int value){
+	printf("%d\n",value);
+}
+//迭代写法 
+void PreOrder(BinaryTree *root){
+	stack<BinaryTree *> s;
+	BinaryTree *bt = root;
+	if(root == nullptr)
+	    return;
+	while(bt != nullptr || !s.empty()){
+		while(bt!=nullptr){
+			print(bt->value);
+			s.push(bt);
+			bt = bt->left;
+		}
+		if(!s.empty()){
+			bt = s.top();
+			s.pop();
+			bt = bt->right;
+		}
+		
+	}
+}
+
+//递归写法
+void PreOrder2(BinaryTree *root) {
+	if(root == nullptr)
+	    return;
+	print(root->value);
+	PreOrder2(root->left);
+	PreOrder2(root->right);
+}
